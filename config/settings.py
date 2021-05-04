3d23be2fd650c592d206c17fda0ddf894328f01b
Evans Pauliuts <pauliutsevans@gmail.com>
2021-05-04 18:22:02 +0300

    add enviroment and update

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dqf=p$5jwmkllsb7gr019ago%=f73bu20+-9n07!3)dd_u8qu='
+SECRET_KEY = os.environ.get('SECRET_KEY', default='dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default='*').split(' ')
 
 
 # Application definition
